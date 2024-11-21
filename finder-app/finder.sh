99c956a625e375f215ca46d0aedfd4ecbb7834e0
root <root@DESKTOP-NMIQB27.localdomain>
2024-11-22 08:05:14 +0900

    finder

---
+++ finder-app/finder.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Insufficient arguments."
+    exit 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ ! -d $filesdir ]; then
+    echo "$filesdir   doesn not edxist or is not a directory."
+    exit 1
+fi
+
+fileNum=0
+lineNum=0
+
+for file in $filesdir; do
+    echo $file
+    lineNumTemp=$(grep -c $searchstr $file)
+    if [ $lineNumTemp > 0 ]; then
+        fileNum=$(( $fileNum+1 ))
+        lineNum=$(( $lineNum+$lineNumTemp ))
+    fi
+done
+
+echo "The number of files are $fileNum and the number of matching lines are $lineNum."
