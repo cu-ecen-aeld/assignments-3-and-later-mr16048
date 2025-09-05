a351da97890135ca04bab360ccf28c864e158cb7
rmori <moriryosuke48@gmail.com>
2025-09-05 16:49:34 +0900

    fixed index search

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -82,7 +82,8 @@ int aesd_circular_buffer_find_entry_offset_and_index_for_fpos(struct aesd_circul
 	total_len = 0;
 	PDEBUG("aesd_circular_buffer_find_index(): out_ofs = %d, in_ofs = %d, char_offset = %d", buffer->out_offs, buffer->in_offs, char_offset);
 	
-	i = buffer->in_offs;
+	// i = buffer->in_offs;
+	i = 0;
 	while(1){
 		if(total_len + buffer->entry[i].size > char_offset){
 			*entry_offset_byte_rtn = char_offset - total_len;
